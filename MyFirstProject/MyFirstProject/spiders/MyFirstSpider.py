916697b2f3f29c5288c6e347483ec85af00c8db8
ridiculousboy <ridiculosboy@gmail.com>
2023-12-10 20:39:32 +0500

    scraped all pages

--- MyFirstProject/MyFirstProject/spiders/MyFirstSpider.py
+++ MyFirstProject/MyFirstProject/spiders/MyFirstSpider.py
@@ -19,3 +19,11 @@ class MyfirstspiderSpider(scrapy.Spider):
                 "price" : book.css("div.product_price .price_color::text").get(),
                 "availability" : book.css("div.product_price p.instock.availability")[0].xpath("string()").get()
             }
+        
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com/"+next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+            yield response.follow(next_page_url, callback=self.parse)
